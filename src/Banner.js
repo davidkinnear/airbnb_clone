bd0e8f12ea012f6aeb29f754d978c47105e73b08
David Kinnear <66265062+davidkinnear@users.noreply.github.com>
2020-09-08 23:07:00 -0500

    Update Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -7,13 +7,15 @@ import { useHistory } from "react-router-dom";
 
 function Banner() {
     const history=useHistory();
+//showSearch keeps track of whether the button is clicked or not    
     const [showSearch,setShowSearch] = useState(false);
 
     return (
         <div className="banner">
-            <div className="banner__search">
-                {showSearch && <Search/>}
-
+            <div className="banner__search"> 
+ {/* if showSearch is true render the Search element*/}                          
+                {showSearch && <Search/>} 
+{/*when click button the opposite of showSearch executes*/}
     <Button onClick={() => setShowSearch(!showSearch)} className="banner__searchButton" variant="outlined">{showSearch ? "Hide":"Search Dates"}</Button>
             </div>
             <div className="banner__info">
